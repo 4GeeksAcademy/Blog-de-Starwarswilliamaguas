5ea396d6e250a6ba0087b93589540141f0e16c9c
William Aguas <148153196+WILLIAMAGUAS@users.noreply.github.com>
2025-05-29 02:54:31 +0000

    primera parte

---
+++ src/components/TarjetaPersonaje.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const TarjetaPersonaje = (props) => {
+    return (
+        <div className="card" style={{ width: "18rem" }}>
+            <img src={`https://raw.githubusercontent.com/breatheco-de/swapi-images/refs/heads/master/public/images/people/${props.personaje.uid}.jpg`} className="card-img-top"></img>
+            <div className="card-body">
+                <h5 className="card-title">{props.personaje.name}</h5>
+               <div className= "d-flex justify-content-between">
+                <Link to={"/"} className='btn btn-primary'>
+                learn more
+                </Link>
+                <button className='btn btn-outline-warning'>
+                    <i className="fa-regular fa-heart"></i>
+                </button>
+
+               </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default TarjetaPersonaje
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,66 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import TarjetaPersonaje from "../components/TarjetaPersonaje.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import TarjetaPlaneta from "../components/TarjetaPlaneta.jsx"
+import TarjetaVehiculo from "../components/TarjetaVehiculo.jsx";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
+	const [personajes, setPersonajes] = useState([]);
+	const [planetas, setPlanetas] = useState([]);
+	const [vehiculos, setVehiculos] = useState([]);
+
+	useEffect(() => {
+		fetch("https://www.swapi.tech/api/people")
+			.then(res => res.json())
+			.then(data => setPersonajes(data.results))
+			.catch(err => console.error(err))
+
+
+		fetch("https://www.swapi.tech/api/planets")
+			.then(res => res.json())
+			.then(data => setPlanetas(data.results))
+			.catch(err => console.error(err))
+			
+		fetch("https://www.swapi.tech/api/vehicles")
+			.then(res => res.json())
+			.then(data => setVehiculos(data.results))
+			.catch(err => console.error(err))		
+
+	}, []);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="container d-flex flex-column w-100">
+			<div className="d-flex flex-column w-100">
+				<h2>Personajes</h2>
+				<div className="d-flex  w-100 gap-4 flex-wrap">
+					{personajes.map((personaje, index) => (
+						<TarjetaPersonaje key={index} personaje={personaje} />
+					))}
+					
+				</div>
+			</div>
+
+			<div className="d-flex flex-column w-100">
+				<h2>Planetas</h2>
+				<div className="d-flex  w-100 gap-4">
+					<TarjetaPlaneta planeta={{ name: "Tierra" }} />
+					<TarjetaPlaneta planeta={{ name: "Marte" }} />
+					<TarjetaPlaneta planeta={{ name: "Venus" }} />
+				</div>
+			</div>
+
+			<div className="d-flex flex-column w-100">
+				<h2>Vehiculos</h2>
+				<div className="d-flex  w-100 gap-4">
+					<TarjetaVehiculo vehiculo={{ name: "nave1" }} />
+					<TarjetaVehiculo vehiculo={{ name: "nave2" }} />
+					<TarjetaVehiculo vehiculo={{ name: "nave3" }} />
+				</div>
+			</div>
+
 		</div>
 	);
 }; 
